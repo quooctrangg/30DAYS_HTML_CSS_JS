fae98d31245c0e6f7aac3ef4dbe3d1b2f7667f54
quoctrang <quoctrang@quoctrang.quoctrang>
2022-12-07 18:50:21 +0700

    day 3

--- day_3-Modal_Popup/index.js
+++ day_3-Modal_Popup/index.js
@@ -2,7 +2,13 @@ let btnOpen = document.getElementsByClassName('open_modal_btn')[0];
 let modal = document.getElementsByClassName('hide')[0];
 let btn_close = document.getElementsByClassName('btn_close')[0];
 let icon_close = document.getElementsByClassName('icon_close')[0];
+let body = document.getElementsByTagName('body')[0]
 
+body.addEventListener('click', (e) => {
+    if (e.target === body) {
+        modal.style.display = 'none'
+    }
+})
 
 btnOpen.addEventListener('click', () => {
     modal.style.display = 'inline'
